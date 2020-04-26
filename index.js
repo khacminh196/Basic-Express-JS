7b666cbf611a58dab800f9439e20a485e4473c74
KhacMinh <khacminh196@gmail.com>
2020-04-26 20:25:40 +0700

    Signed Cookie

--- index.js
+++ index.js
@@ -7,21 +7,22 @@ var userRouter = require('./Routers/user.router');
 var productRouter = require('./Routers/product.router');
 var authRouter = require('./Routers/auth.router');
 var loginMiddlewares = require('./middlewares/userLogin.middleware');
+var cookieParser = require('cookie-parser');
 
-app.use(cookieParser());
 app.use(express.json()) // for parsing application/json
 app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
+app.use(cookieParser('thisiscookie'));
 app.use(express.static('public'))
 
 app.set('view engine', 'pug');
 app.set('Views', './Views');
 
-app.get('/', (req, res) => {
+app.get('/', loginMiddlewares, (req, res) => {
 	res.render('index');
 });
 
-app.use('/users', userRouter);
+app.use('/users',loginMiddlewares, userRouter);
 app.use('/products', loginMiddlewares, productRouter);
 app.use('/auth', authRouter);
 
--- middlewares/userLogin.middleware.js
+++ middlewares/userLogin.middleware.js
@@ -2,17 +2,18 @@ var db = require('../db.js');
 
 module.exports = (req, res, next) => {
 	// Kiểm tra xem có người dùng đăng nhập không
-	if(!req.cookies.userId) {
+	if(!req.signedCookies.userId) {
 		res.redirect('auth/login');
 		return;
 	}
 
-	var user = db.get('users').find({ id : req.cookies.userId }).value();
+	var user = db.get('users').find({ id : req.signedCookies.userId }).value();
 	// Kiểm tra có phải người dùng thật không
 	if(!user) {
 		res.redirect('auth/login');
 		return;
 	}
 
+	res.locals.user = user;
 	next();
 };
